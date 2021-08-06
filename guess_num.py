2f2402d4ce4c37724ca447d1864683e2895a0d73
Chen Rong <rong19036@gmail.com>
2021-08-06 17:06:54 +0800

    add count

--- guess_num.py
+++ guess_num.py
@@ -1,14 +1,19 @@
 import random
 
 r = random.randint(1,20)
+count = 0
 while True:
     num = input('猜數字:')
     num = int(num)
     if num == r:
         print('bingo!!!')
+        print('這是第', count, '次')
         break
     elif num > r:
         print('lower')
+        count += 1
     elif num < r:
         print('higher')
+        count += 1
+    print('這是第', count, '次')
         
